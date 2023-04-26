4dfb98fb4e3259416a7b6dc7593ae5f8df24029f
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-04-26 19:46:34 +0100

    styling changes to following feed

--- hertz/src/components/FollowingFeed.tsx
+++ hertz/src/components/FollowingFeed.tsx
@@ -29,6 +29,30 @@ const FollowingFeed: React.FC = () => {
             artist: 'Artist Name 4',
             audioUrl: 'https://example.com/audio-url-2.mp3',
         },
+        {
+            imageUrl: 'https://via.placeholder.com/300',
+            title: 'Shared Song 4',
+            artist: 'Artist Name 4',
+            audioUrl: 'https://example.com/audio-url-2.mp3',
+        },
+        {
+            imageUrl: 'https://via.placeholder.com/300',
+            title: 'Shared Song 5',
+            artist: 'Artist Name 5',
+            audioUrl: 'https://example.com/audio-url-2.mp3',
+        },
+        {
+            imageUrl: 'https://via.placeholder.com/300',
+            title: 'Shared Song 6',
+            artist: 'Artist Name 6',
+            audioUrl: 'https://example.com/audio-url-2.mp3',
+        },
+        {
+            imageUrl: 'https://via.placeholder.com/300',
+            title: 'Shared Song 7',
+            artist: 'Artist Name 7',
+            audioUrl: 'https://example.com/audio-url-2.mp3',
+        },
     ];
 
     return (
