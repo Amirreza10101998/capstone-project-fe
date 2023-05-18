646d49c9af8bfb262910922d476717cbb0b155d9
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-05-18 13:10:38 +0100

    playlists can be added from user profile page

--- hertz/src/components/ProfilePage.tsx
+++ hertz/src/components/ProfilePage.tsx
@@ -1,29 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import PlaylistModal from './PlaylistModal';
-import NewPlaylistModal from './NewPlaylistModal';
 import { Dialog, Transition } from '@headlessui/react';
-
-type Song = {
-    id: string;
-    title: string;
-    artist: string;
-};
 
 type Playlist = {
     id: string;
+    user_id: string;
     name: string;
-    songs: Song[];
-};
+    description: string | null;
+    SongCardId: string | null;
+    createdAt: string;
+    updatedAt: string;
+    User: {
+        id: string;
+        username: string;
+        avatar: string | null;
+    };
+    SongCards: any[];
+}
+
 
 type ProfileState = {
     id: string;
     email: string;
     username: string;
-    avatar: string | null;
+    avatar: null | string;
     favorite_genres: string[];
     favorite_artists: string[];
-    playlists: Playlist[];
-}
+    refreshToken: string;
+    googleId: null | string;
+};
+
 
 
 const ProfilePage: React.FC = () => {
@@ -34,12 +39,16 @@ const ProfilePage: React.FC = () => {
         avatar: null,
         favorite_genres: [],
         favorite_artists: [],
-        playlists: [],
+        refreshToken: '',
+        googleId: null
     });
 
-    const [selectedPlaylist, setSelectedPlaylist] = useState<Playlist | null>(null);
     const [isNewPlaylistModalOpen, setIsNewPlaylistModalOpen] = useState(false);
     const [isUpdated, setIsUpdated] = useState(false);
+    const [playlistName, setPlaylistName] = useState("");
+    const [playlists, setPlaylists] = useState<Playlist[]>([]);
+
+
 
     const createNewPlaylist = async (name: string) => {
         const token = localStorage.getItem('accessToken');
@@ -55,14 +64,36 @@ const ProfilePage: React.FC = () => {
 
         if (response.ok) {
             const newPlaylist = await response.json();
-            setProfile((prevProfile) => ({
-                ...prevProfile,
-                playlists: [...prevProfile.playlists, newPlaylist],
-            }));
+            setPlaylists(prevPlaylists => [...prevPlaylists, newPlaylist]);
         } else {
             console.error('Error creating new playlist:', response.statusText);
         }
     };
+
+
+
+
+    useEffect(() => {
+        const fetchPlaylists = async () => {
+            const token = localStorage.getItem('accessToken');
+            const apiUrl = process.env.REACT_APP_BE_URL;
+            const response = await fetch(`${apiUrl}/playlist`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                const playlists = await response.json();
+                setPlaylists(playlists); // <-- change this line
+                console.log('Fetched Playlists:', playlists);
+            } else {
+                console.error('Error fetching playlists:', response.statusText);
+            }
+        };
+
+        fetchPlaylists();
+    }, []);
 
 
     useEffect(() => {
@@ -95,11 +126,6 @@ const ProfilePage: React.FC = () => {
 
         fetchProfile();
     }, []);
-
-    useEffect(() => {
-        console.log('isNewPlaylistModalOpen', isNewPlaylistModalOpen);
-    }, [isNewPlaylistModalOpen]);
-
 
 
     const updateProfile = async (updatedProfile: Partial<ProfileState>) => {
@@ -142,12 +168,6 @@ const ProfilePage: React.FC = () => {
         updateProfile(updatedProfile);
     };
 
-    const handlePlaylistClick = (playlist: Playlist) => {
-        setSelectedPlaylist(playlist);
-        setIsNewPlaylistModalOpen(true);
-        console.log(isNewPlaylistModalOpen);
-    };
-
     return (
         <>
             <div className="bg-black min-h-screen flex items-center">
@@ -178,65 +198,80 @@ const ProfilePage: React.FC = () => {
                         <div className="mb-6">
                             <h2 className="text-white text-3xl font-bold mb-4 mt-4">Playlists</h2>
                             <button onClick={() => setIsNewPlaylistModalOpen(true)} className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4 hover:bg-blue-600 w-full">Create New Playlist</button>
-                            <table className="w-full bg-gray-800 text-white mt-4">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {profile.playlists?.map(playlist => (
-                                        <tr key={playlist.id} onClick={() => handlePlaylistClick(playlist)}>
-                                            <td>{playlist.name}</td>
+
+                            {playlists && playlists.length > 0 ? (
+                                <table className="table-auto bg-white text-black w-full mt-4">
+                                    <thead>
+                                        <tr>
+                                            <th className="px-4 py-2">ID</th>
+                                            <th className="px-4 py-2">Name</th>
+                                            <th className="px-4 py-2">Songs</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        {playlists.map((playlist, index) => (
+                                            <tr key={index} className={index % 2 === 0 ? 'bg-gray-200' : ''}>
+                                                <td className="border px-4 py-2">{playlist.id}</td>
+                                                <td className="border px-4 py-2">{playlist.name}</td>
+                                                <td className="border px-4 py-2">{playlist.SongCards ? playlist.SongCards.length : 0}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            ) : (
+                                <p className="text-white mt-4">No playlists yet</p>
+                            )}
+
+
                         </div>
-                        {selectedPlaylist && <PlaylistModal playlist={selectedPlaylist} onClose={() => setSelectedPlaylist(null)} />}
-
-                        <Transition show={isNewPlaylistModalOpen} as={React.Fragment}>
-                            <Dialog
-                                as="div"
-                                className="fixed inset-0 z-10 overflow-y-auto"
-                                onClose={() => setIsNewPlaylistModalOpen(false)}
-                            >
-                                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                                    <Transition.Child
-                                        as={React.Fragment}
-                                        enter="ease-out duration-300"
-                                        enterFrom="opacity-0"
-                                        enterTo="opacity-100"
-                                        leave="ease-in duration-200"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <Dialog.Overlay className="fixed inset-0 transition-opacity" />
-                                    </Transition.Child>
-
-                                    {/* This element is to trick the browser into centering the modal contents. */}
-                                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-
-                                    <Transition.Child
-                                        as={React.Fragment}
-                                        enter="ease-out duration-300"
-                                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                        enterTo="opacity-100 translate-y-0 sm:scale-100"
-                                        leave="ease-in duration-200"
-                                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                    >
-                                        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                                            <NewPlaylistModal onClose={() => setIsNewPlaylistModalOpen(false)} onCreate={createNewPlaylist} />
-                                        </div>
-                                    </Transition.Child>
-                                </div>
-                            </Dialog>
-                        </Transition>
-
                     </div>
                 </div>
             </div>
+
+            <Transition show={isNewPlaylistModalOpen} as={React.Fragment}>
+                <Dialog open={isNewPlaylistModalOpen} onClose={() => setIsNewPlaylistModalOpen(false)} className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title">
+                    <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:p-0">
+                        <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-75 transition-opacity" />
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                        <Transition.Child
+                            enter="transition-opacity duration-500 ease-in-out"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="transition-opacity duration-500 ease-in-out"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="inline-block bg-gray-800 text-white rounded-lg text-left overflow-hidden shadow-xl transform transition-opacity sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                                <div className="flex justify-between items-center border-b border-gray-700 p-6">
+                                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-white">
+                                        Create New Playlist
+                                    </Dialog.Title>
+                                    <button onClick={() => setIsNewPlaylistModalOpen(false)} className="ml-3 bg-transparent border-none">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-300 hover:text-white">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <form onSubmit={(e) => {
+                                    e.preventDefault();
+                                    createNewPlaylist(playlistName);
+                                    setIsNewPlaylistModalOpen(false);
+                                }} className="px-6 py-4 bg-gray-700">
+                                    <input
+                                        value={playlistName}
+                                        onChange={(e) => setPlaylistName(e.target.value)}
+                                        className="w-full px-3 py-2 text-sm text-gray-700 bg-gray-800 rounded"
+                                        placeholder="Enter playlist name..."
+                                        required
+                                    />
+                                    <button type="submit" className="px-4 py-2 mt-2 text-sm text-white bg-blue-500 hover:bg-blue-700 rounded">Create</button>
+                                </form>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition>
+
         </>
     );
 };
